d724c9f29a344a9ca8c2954b9e5289fc9a94adf9
preet kaur <preetjandu0003@gmail.com>
2020-01-20 22:32:18 -0500

    Initial Commit

---
+++ Assignment2_761984/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment2_761984
+//
+//  Created by Megha Mahna on 2020-01-20.
+//  Copyright © 2020 preet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
